4d1ba207f842cbb3bc61287682cec933e5774731
MauriDV <mdellevedove12@gmail.com>
2017-01-30 02:45:11 -0300

    add comments

--- calculator.js
+++ calculator.js
@@ -1,14 +1,17 @@
 function Calculator () {
 }
 
+//Suma
 Calculator.prototype.sum = function(x,y) {
 	return (x+y);
 }
 
+//Resta
 Calculator.prototype.rest = function(x,y) {
 	return (x-y);
 };
 
+//Multiplicacion
 Calculator.prototype.mul = function(x,y) {
 	return (x*y);
 }
